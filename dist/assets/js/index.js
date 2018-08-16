939f30e81cd99c2f9d6960c4275293e3e3bb4505
Gautam.Naik <gautam.naik@bidchat.com>
2018-08-16 15:40:05 +0530

    Added summary

--- dist/assets/js/index.js
+++
@@ -1,9 +0,0 @@
-fetch(
-  'https://api.stackexchange.com/2.2/users/2376317?order=desc&sort=reputation&site=stackoverflow',
-)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
