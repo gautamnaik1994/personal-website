f5ff4a27b07bcdebe9ad0cb40ba78610202e39f1
Gautam Naik <gautamnaik1994@gmail.com>
2018-08-19 16:43:18 +0530

    aaded more skills and softwares

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,27 @@
-fetch(
-  'https://api.stackexchange.com/2.2/users/2376317?order=desc&sort=reputation&site=stackoverflow',
-)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  });
+// fetch(
+//   'https://api.stackexchange.com/2.2/users/2376317?order=desc&sort=reputation&site=stackoverflow',
+// )
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(myJson) {
+//     console.log(myJson);
+//   });
+
+window.onload = function(e) {
+  let expert = document.querySelectorAll('[data-skillLevel="Expert"]');
+  let intermediate = document.querySelectorAll(
+    '[data-skillLevel="Intermediate"]',
+  );
+  let beginner = document.querySelectorAll('[data-skillLevel="Beginner"]');
+  if (expert.length > 0) addSkills(expert);
+  if (intermediate.length > 0) addSkills(intermediate);
+  if (beginner.length > 0) addSkills(beginner);
+};
+function addSkills(nodes) {
+  for (let i = 0; i < nodes.length; i++) {
+    let skillNodeValue = nodes[i].attributes['data-skillLevel'].nodeValue;
+    nodes[i].textContent = skillNodeValue;
+    //nodes[i].classList.add(skillNodeValue.toLowerCase() + '');
+  }
+}
