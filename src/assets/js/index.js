e64727bd020a88396bc3b0509f3996507981eda5
Gautam Naik <gautamnaik1994@gmail.com>
2018-08-23 02:19:27 +0530

    added prompt

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -20,22 +20,22 @@ window.onload = function (e) {
   if (expert.length > 0) addSkills(expert);
   if (intermediate.length > 0) addSkills(intermediate);
   if (beginner.length > 0) addSkills(beginner);
-  installBtn = document.querySelector('#installApp');
-  installBtn.addEventListener('click', (e) => {
+  //installBtn = document.querySelector('#installApp');
+  // installBtn.addEventListener('click', (e) => {
 
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+  //   // Show the prompt
+  //   deferredPrompt.prompt();
+  //   // Wait for the user to respond to the prompt
+  //   deferredPrompt.userChoice
+  //     .then((choiceResult) => {
+  //       if (choiceResult.outcome === 'accepted') {
+  //         console.log('User accepted the A2HS prompt');
+  //       } else {
+  //         console.log('User dismissed the A2HS prompt');
+  //       }
+  //       deferredPrompt = null;
+  //     });
+  // });
 };
 
 function addSkills(nodes) {
@@ -70,6 +70,15 @@ function init() {
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
       deferredPrompt.prompt();
+      deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
     });
 
     window.addEventListener('appinstalled', (evt) => {
