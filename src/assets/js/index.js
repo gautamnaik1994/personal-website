95f627349557c07ca90d2e15344c41a5e1316235
Gautam Naik <gautamnaik1994@gmail.com>
2018-08-23 02:42:12 +0530

    indented

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -71,14 +71,14 @@ function init() {
       deferredPrompt = e;
       deferredPrompt.prompt();
       deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+        .then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
     });
 
     window.addEventListener('appinstalled', (evt) => {
@@ -87,7 +87,7 @@ function init() {
     });
 
 
-    
+
   }
 }
 
