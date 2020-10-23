789150de8cc71713878d8d3f33c82fa41c4cbaf0
Gautam.Naik <gautam.naik@bidchat.com>
2020-10-24 02:34:18 +0530

    added 3d

--- src/components/SubContainer.tsx
+++ src/components/SubContainer.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import media from '../utils/MediaQueries';
 
-const Container = styled.div`
+const SubContainer = styled.div`
   width: 100%;
   max-width: 100%;
-  padding: 0 15px;
   margin: 0 auto;
   ${media.tablet} {
-    max-width: 1140px;
+    max-width: 768px;
   }
 `;
 
 interface Props {
   children: React.ReactNode;
 }
-export default ({ children }: Props) => <Container>{children}</Container>;
+export default ({ children }: Props) => <SubContainer>{children}</SubContainer>;
